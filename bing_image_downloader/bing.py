38070ce9652b0cfc995ccb961b9fd8519b4a7739
Dongbin Na <ndb796@naver.com>
2021-02-21 20:21:31 +0900

    Update bing.py

--- bing_image_downloader/bing.py
+++ bing_image_downloader/bing.py
@@ -33,7 +33,7 @@ class Bing:
         request = urllib.request.Request(link, None, self.headers)
         image = urllib.request.urlopen(request, timeout=self.timeout).read()
         if not imghdr.what(None, image):
-            print('[Error]Invalid image, not saving {}\n'.format(link))
+            print('[Error] Invalid image, not saving {}'.format(link))
             raise
         with open(file_path, 'wb') as f:
             f.write(image)
@@ -50,18 +50,18 @@ class Bing:
                 file_type = "jpg"
 
             # Download the image
-            print("[%] Downloading Image #{} from {}".format(self.download_count, link))
+            print("#{} 이미지 다운로드 ({})".format(self.download_count, link))
 
-            self.save_image(link, "{}/{}/{}/".format(os.getcwd(), self.output_dir, self.query) + "Image_{}.{}".format(
+            self.save_image(link, "{}/{}/{}/".format(os.getcwd(), self.output_dir, self.query) + "image_{}.{}".format(
                 str(self.download_count), file_type))
-            print("[%] File Downloaded !\n")
+            print("#{} 파일 다운로드가 완료되었습니다.".format(self.download_count))
         except Exception as e:
             self.download_count -= 1
-            print("[!] Issue getting: {}\n[!] Error:: {}".format(link, e))
+            print("[Info] Issue getting: {}\n[Error] {}".format(link, e))
 
     def run(self):
         while self.download_count < self.limit:
-            print('\n\n[!!]Indexing page: {}\n'.format(self.page_counter + 1))
+            print('[Info] Indexing page: {}'.format(self.page_counter + 1))
             # Parse the page source and download pics
             request_url = 'https://www.bing.com/images/async?q=' + urllib.parse.quote_plus(self.query) \
                           + '&first=' + str(self.page_counter) + '&count=' + str(self.limit) \
@@ -71,15 +71,15 @@ class Bing:
             html = response.read().decode('utf8')
             links = re.findall('murl&quot;:&quot;(.*?)&quot;', html)
 
-            print("[%] Indexed {} Images on Page {}.".format(len(links), self.page_counter + 1))
-            print("\n===============================================\n")
+            print("[Info] Indexed {} Images on Page {}.".format(len(links), self.page_counter + 1))
+            print("===============================================")
 
             for link in links:
                 if self.download_count < self.limit:
                     self.download_image(link)
                 else:
-                    print("\n\n[%] Done. Downloaded {} images.".format(self.download_count))
-                    print("\n===============================================\n")
+                    print("===============================================")
                     break
+            print("[Info] Done. Downloaded {} images.".format(self.download_count))
 
             self.page_counter += 1
